f2723a1f6b21842dc7495fbcd9c6ded60ab37450
Augustine Namare <agNamare@gmail.com>
2024-09-28 12:16:23 +0300

    Change server to production

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASEURL = "http://127.0.0.1:3000";
+const BASEURL = "https://ecom-backend-qdwv.onrender.com";
 
 export default axios.create({
   baseURL: BASEURL,
@@ -8,7 +8,7 @@ export default axios.create({
 });
 
 export const axiosPrivate = axios.create({
-  baseURL: "http://127.0.0.1:3000", // Ensure this matches your server's URL
+  baseURL: "https://ecom-backend-qdwv.onrender.com", // Ensure this matches your server's URL
   headers: { "Content-Type": "application/json" },
   withCredentials: true, // Include credentials (cookies) with requests
 });
